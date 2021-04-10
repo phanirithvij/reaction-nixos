83b97f6a75707b9bcf86d1046dca929279abef26
ppom <ppom@ppom.me>
2021-04-10 15:40:49 +0200

    move nginx & ssl conf to websites.nix

--- configuration.nix
+++ configuration.nix
@@ -120,7 +120,6 @@ in
   networking.firewall.enable = true;
   networking.firewall.allowedTCPPorts = [
     sshPort
-    80 443 # web
   ];
 
   # Fail2ban service
@@ -133,97 +132,6 @@ in
     findtime = 1200
     bantime = 2400
   '';
-
-  # Nginx
-  services.nginx = {
-    enable = true;
-    enableReload = true;
-    clientMaxBodySize = "15G";
-    # Enable all recommended settings
-    recommendedTlsSettings = true;
-    recommendedGzipSettings = true;
-    recommendedOptimisation = true;
-    recommendedProxySettings = true;
-    appendConfig = ''
-      worker_processes auto;
-    '';
-
-    # Hosts config
-    virtualHosts = {
-      "ppom.me" = {
-        # makes it the default host
-        default = true;
-        # enable and force SSL with Let's Encrypt
-        forceSSL = true;
-        enableACME = true;
-        locations = {
-          "/" = {
-            index = "index.html";
-            root = "/var/www/musi";
-          };
-        };
-        extraConfig = ''
-          if ($host = veloc.ppom.me) {
-            return 301 https://assos.utc.fr/veloc/$request_uri;
-          } #
-        '';
-      };
-
-      "u.ppom.me" = {
-        # enable and force SSL with Let's Encrypt
-        forceSSL = true;
-        enableACME = true;
-        # locations
-        locations = {
-          "/" = {
-            index = "index.html";
-            root = "/data/uploader";
-            extraConfig = "autoindex on;";
-            #extraConfig = '' fancyindex on; fancyindex_exact_size off; '';
-          };
-        };
-      };
-
-      # "xn--morts_annonces-mkb.ppom.me" = {
-      #   forceSSL = true;
-      #   enableACME = true;
-      #   locations = {
-      #     "/" = {
-      #       index = "/data/uploader/morts_annoncees.mp4";
-      #       root = "/var/empty";
-      #     };
-      #   };
-      # };
-
-      "veloc.ppom.me" = {
-        extraConfig = ''
-            return 301 https://assos.utc.fr/veloc/$request_uri;
-        '';
-        # enable and force SSL with Let's Encrypt
-        forceSSL = true;
-        enableACME = true;
-      };
-
-      "blog.ppom.me" = {
-        # enable and force SSL with Let's Encrypt
-        forceSSL = true;
-        enableACME = true;
-        # locations
-        locations = {
-          "/" = {
-            tryFiles = "$uri $uri.html $uri/ =404";
-            root = "/data/blog";
-          };
-        };
-      };
-    };
-  };
-
-  # Let's Encrypt config
-  security.acme = {
-    acceptTerms = true;
-    email = "paco@ecomail.io";
-  };
 
   services.rzw = {
     enable = true;
---
+++ websites.nix
@@ -0,0 +1,99 @@
+{ config, pkgs, ... }:
+
+{
+  networking.firewall.allowedTCPPorts = [
+    80 443 # web
+  ];
+
+  # Nginx
+  services.nginx = {
+    enable = true;
+    enableReload = true;
+    clientMaxBodySize = "15G";
+    # Enable all recommended settings
+    recommendedTlsSettings = true;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    appendConfig = ''
+      worker_processes auto;
+    '';
+
+    # Hosts config
+    virtualHosts = {
+      "ppom.me" = {
+        # makes it the default host
+        default = true;
+        # enable and force SSL with Let's Encrypt
+        forceSSL = true;
+        enableACME = true;
+        locations = {
+          "/" = {
+            index = "index.html";
+            root = "/var/www/musi";
+          };
+        };
+        extraConfig = ''
+          if ($host = veloc.ppom.me) {
+            return 301 https://assos.utc.fr/veloc/$request_uri;
+          } #
+        '';
+      };
+
+      "u.ppom.me" = {
+        # enable and force SSL with Let's Encrypt
+        forceSSL = true;
+        enableACME = true;
+        # locations
+        locations = {
+          "/" = {
+            index = "index.html";
+            root = "/data/uploader";
+            extraConfig = "autoindex on;";
+            #extraConfig = '' fancyindex on; fancyindex_exact_size off; '';
+          };
+        };
+      };
+
+      # "xn--morts_annonces-mkb.ppom.me" = {
+      #   forceSSL = true;
+      #   enableACME = true;
+      #   locations = {
+      #     "/" = {
+      #       index = "/data/uploader/morts_annoncees.mp4";
+      #       root = "/var/empty";
+      #     };
+      #   };
+      # };
+
+      "veloc.ppom.me" = {
+        extraConfig = ''
+            return 301 https://assos.utc.fr/veloc/$request_uri;
+        '';
+        # enable and force SSL with Let's Encrypt
+        forceSSL = true;
+        enableACME = true;
+      };
+
+      "blog.ppom.me" = {
+        # enable and force SSL with Let's Encrypt
+        forceSSL = true;
+        enableACME = true;
+        # locations
+        locations = {
+          "/" = {
+            tryFiles = "$uri $uri.html $uri/ =404";
+            root = "/data/blog";
+          };
+        };
+      };
+    };
+  };
+
+  # Let's Encrypt config
+  security.acme = {
+    acceptTerms = true;
+    email = "paco@ecomail.io";
+  };
+
+}
