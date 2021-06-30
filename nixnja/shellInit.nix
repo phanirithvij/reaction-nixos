a9fedae484df859163f66e287f834f4553b2aa6a
ppom <ppom@ppom.me>
2021-06-30 10:19:12 +0200

    add nixnja = nix golf

---
+++ nixnja/shellInit.nix
@@ -0,0 +1,9 @@
+{ pkgs }:
+{
+  programs.bash.shellInit = ''
+    [ -f ${pkgs.fzf}/share/fzf/completion.bash ] && source ${pkgs.fzf}/share/fzf/completion.bash
+    [ -f ${pkgs.fzf}/share/fzf/key-bindings.bash ] && source ${pkgs.fzf}/share/fzf/key-bindings.bash
+  '';
+  programs.bash.shellInit = lib.concatMapStrings (file: let path = "${pkgs.fzf}/share/fzf/${file}"; in "[ -f ${path} ] && source ${path}\n") [ "completion.bash" "key-bindings.bash" ];
+  programs.bash.shellInit = lib.concatMapStrings (file: "[ -f ${path} ] && source ${path}\n") (map (file: "${pkgs.fzf}/share/fzf/${file}") [ "completion.bash" "key-bindings.bash" ]);
+}
