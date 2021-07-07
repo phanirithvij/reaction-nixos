ce5f13ac2180ee270d1123baff3a3a6ba6639a82
ppom <ppom@ppom.me>
2021-07-07 16:06:49 +0200

    Manage nvim plugins via nix

--- modules/common/nvim.nix
+++ modules/common/nvim.nix
@@ -1,6 +1,32 @@
 { lib, config, pkgs, ... }:
 
 with config.ppom;
+let
+  undoquit-vim = pkgs.vimUtils.buildVimPluginFrom2Nix {
+    pname = "undoquit-vim";
+    version = "2021-05-16";
+    src = pkgs.fetchFromGitHub {
+      owner = "AndrewRadev";
+      repo = "undoquit.vim";
+      rev = "74d2a1fc51af91d8b758ad85fb236b6476c8ed0c";
+      sha256 = "1a5h0iwmk3fd5qs8wa7v9kys8k2q52jwp71s8c4j0z6c5k1173wn";
+    };
+    meta.homepage = "https://github.com/AndrewRadev/undoquit.vim";
+  };
+
+  vim-svelte = pkgs.vimUtils.buildVimPluginFrom2Nix {
+    pname = "vim-svelte";
+    version = "2021-04-21";
+    src = pkgs.fetchFromGitHub {
+      owner = "evanleck";
+      repo = "vim-svelte";
+      rev = "5f88e5a0fe7dcece0008dae3453edbd99153a042";
+      sha256 = "0p941kcqnv4wgcybmhnpzrvxm2y9d2fkd4n186zav7mwfzn736jq";
+    };
+    meta.homepage = "https://github.com/evanleck/vim-svelte";
+  };
+
+in
 {
   imports = [ ./ppom.nix ];
 
@@ -34,6 +60,16 @@ with config.ppom;
             nnoremap <leader>h noh<CR>
             colorscheme gruvbox
           '' + lib.optionalString isDesktop ''
+            let g:languagetool_jar='${pkgs.languagetool}/share/languagetool-commandline.jar'
+
+            " Function to source only if file exists {
+            function! SourceIfExists(file)
+              if filereadable(expand(a:file))
+                exe 'source' a:file
+              endif
+            endfunction
+            " }
+            call SourceIfExists("~/.config/nvim/init.vim")
           '';
           packages.myVimPackage = with pkgs.vimPlugins; {
             start = [
@@ -43,11 +79,24 @@ with config.ppom;
               vim-repeat
               vim-fugitive
               vim-gitgutter
+              vim-unimpaired
               # gruvbox
               (super.vimPlugins.gruvbox.overrideAttrs (oldAttrs: {
                 patches = [ ./true_black_gruvbox.patch ];
               }))
             ] ++ lib.optionals isDesktop [
+              vim-startify
+              fzf-vim
+              far-vim
+              LanguageTool-nvim
+              vim-grammarous
+              unicode-vim
+              # coc-nvim
+              # manuals:
+              undoquit-vim
+              vim-svelte
+              rust-vim
+              vim-markdown
             ];
             opt = [ ];
           };
