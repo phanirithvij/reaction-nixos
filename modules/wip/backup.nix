f84c8664e1a20d41e0773a86e5069a36ecaa175d
ppom <ppom@ppom.me>
2021-07-20 02:14:02 +0200

    Add ready for local ssh backup test

--- modules/wip/backup.nix
+++ modules/wip/backup.nix
@@ -1,32 +1,42 @@
 { lib, pkgs, config, ... }:
 with lib;
-let
-  defaults = {
-      encryption = {
-        mode = ???;
-        passCommand = "cat /prout";
-      };
-      compression = "auto";
-      environment = {
-        BORG_RSH = "ssh -i /path/to/key";
-      };
-      repo = "ssh://user@host:port/path/to/repo";
-  };
-in {
+{
+  # CLIENT
   # backup /data
-  # backup /home
+  # backup /home && /root
   # backup /var
-  # backup /root
-  services.borgbackup.jobs = {
-    slashHome = defaults // {
-      paths = [ "/home" ];
-      startAt = "daily"; # How to specify at night?
-      prune.keep = {
-        within = "1d"; # Keep all archives from the last day
-        daily = 7;
-        weekly = 4;
-        monthly = -1;  # Keep at least one archive for each month
+  services.borgbackup = {
+    jobs = {
+      # Test backup of /etc/nixos
+      etc_nixos = {
+        paths = [ "/etc/nixos/" ];
+        startAt = "daily"; # How to specify at night?
+        prune.keep = {
+          within = "1d"; # Keep all archives from the last day
+          daily = 7;
+          weekly = 4;
+          monthly = -1;  # Keep at least one archive for each month
+        };
+        encryption = {
+          mode = "repokey-blake2";
+          passCommand = "sudo cat /var/secrets/backups/etc_nixos/pass";
+        };
+        compression = "auto";
+        environment = {
+          BORG_RSH = "ssh -i /var/secrets/backups/etc_nixos/key";
+        };
+        repo = "ssh://borg@localhost:${toString (builtins.head config.services.openssh.ports)}/path/to/repo";
+      };
+    };
+
+    repos = {
+      etc_nixos = {
+        authorizedKeys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINOs365Kt31fWF2ovLwmpSWdfsZ8v61OhEaRkD4Altyk root@musi"
+        ] ;
+        path = "/var/lib/backups/etc_nixos" ;
       };
     };
   };
+
 }
