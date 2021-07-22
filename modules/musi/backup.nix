07f5189b3f198c96dd6accf519f092ecba0ac7ae
ppom <ppom@ppom.me>
2021-07-22 03:29:30 +0200

    Add backups <3

--- modules/musi/backup.nix
+++ modules/musi/backup.nix
@@ -1,10 +1,6 @@
 { lib, pkgs, config, ... }:
 with lib;
 {
-  # CLIENT
-  # backup /data
-  # backup /home && /root
-  # backup /var
   services.borgbackup = {
     jobs = {
       # Test backup of /etc/nixos
@@ -27,6 +23,72 @@ with lib;
           BORG_RSH = "ssh -p ${toString (builtins.head config.services.openssh.ports)} -i /var/secrets/backups/etc_nixos/key";
         };
         repo = "borg@localhost:.";
+      };
+
+      # Real backup of /var/
+      var = {
+        paths = [ "/var/" ];
+        # startAt = "daily"; # How to specify at night?
+        startAt = [ "*-*-* 01:00" ];
+        prune.keep = {
+          within = "1d"; # Keep all archives from the last day
+          daily = 7;
+          weekly = 4;
+          monthly = -1;  # Keep at least one archive for each month
+        };
+        encryption = {
+          mode = "repokey-blake2";
+          passCommand = "cat /var/secrets/backups/var/pass";
+        };
+        compression = "lz4";
+        environment = {
+          BORG_RSH = "ssh -i /var/secrets/backups/var/sshkey";
+        };
+        repo = "ppom@node.cmercier.fr:/pacobackup/var";
+      };
+
+      # Real backup of /data/
+      data = {
+        paths = [ "/data/" ];
+        # startAt = "daily"; # How to specify at night?
+        startAt = [ "*-*-* 02:00" ];
+        prune.keep = {
+          within = "1d"; # Keep all archives from the last day
+          daily = 7;
+          weekly = 4;
+          monthly = -1;  # Keep at least one archive for each month
+        };
+        encryption = {
+          mode = "repokey-blake2";
+          passCommand = "cat /var/secrets/backups/data/pass";
+        };
+        compression = "lz4";
+        environment = {
+          BORG_RSH = "ssh -i /var/secrets/backups/data/sshkey";
+        };
+        repo = "ppom@node.cmercier.fr:/pacobackup/data";
+      };
+
+      # Real backup of /home/ & /root/
+      home = {
+        paths = [ "/home/" "/root/" ];
+        # startAt = "daily"; # How to specify at night?
+        startAt = [ "*-*-* 01:30" ];
+        prune.keep = {
+          within = "1d"; # Keep all archives from the last day
+          daily = 7;
+          weekly = 4;
+          monthly = -1;  # Keep at least one archive for each month
+        };
+        encryption = {
+          mode = "repokey-blake2";
+          passCommand = "cat /var/secrets/backups/home/pass";
+        };
+        compression = "lz4";
+        environment = {
+          BORG_RSH = "ssh -i /var/secrets/backups/home/sshkey";
+        };
+        repo = "ppom@node.cmercier.fr:/pacobackup/home";
       };
     };
 
