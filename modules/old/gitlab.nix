f807dd5bdf74e423e7089252afeec8621e18cc53
ppom <ppom@ppom.me>
2021-10-12 09:15:12 +0200

    aborpted attempt to have a gitlab runner

---
+++ modules/old/gitlab.nix
@@ -0,0 +1,13 @@
+{ lib, ... }:
+{
+  # services.gitlab-runner = {
+  #   enable = true;
+  #   concurrent = 4;
+  #   services.ai12 = {
+  #     executor = "docker";
+  #     dockerImage = "python:3.9-alpine";
+  #     maximumTimeout = 60 * 60;
+  #     registrationConfigFile = /var/secrets/gitlab-runner-utc;
+  #   };
+  # };
+}
