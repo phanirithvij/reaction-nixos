6eda081fe2129b9894d2044baddc4d6575228637
ppom <ppom@ppom.me>
2021-11-01 19:11:34 +0100

    Network monitoring script now resides in Nix

--- modules/musi/monitoring.nix
+++ modules/musi/monitoring.nix
@@ -1,5 +1,37 @@
 { config, pkgs, ... }:
 
+let
+  VARS= ''
+    DATA_DIR="$HOME/.check_co/"
+    DATA_FILE="$DATA_DIR/log"
+    TEST_HOST="google.fr"
+  '';
+
+  check = pkgs.writeScriptBin "check_co.sh" ''
+    #!${pkgs.bash}/bin/bash
+
+    ${VARS}
+
+    ${pkgs.coreutils}/bin/mkdir -p $DATA_DIR
+
+    ${pkgs.inetutils}/bin/ping -c2 -W3 $TEST_HOST &>/dev/null
+    STAT=$?
+
+    ${pkgs.coreutils}/bin/echo "$(date +%s) $STAT" | \
+    ${pkgs.coreutils}/bin/tee -a $DATA_FILE
+  '';
+
+  print = pkgs.writeScriptBin "print_co.sh" ''
+    #!${pkgs.bash}/bin/bash
+
+    ${VARS}
+
+    # Print only first column of lines non ending with zero code
+    # | Print the timestamp in a human-readable format
+    ${pkgs.gawk}/bin/awk '! / 0$/ { print $1 }' $DATA_FILE | \
+    ${pkgs.findutils}/bin/xargs -d'\n' -I'{}' date -d'@{}'
+  '';
+in
 {
   services.vnstat.enable = true;
 
@@ -24,11 +56,13 @@
     # Output
   '';
 
+  environment.systemPackages = [ check print ];
+
   # Cron jobs
   services.cron = {
     enable = true;
     systemCronJobs = [
-      ''* * * * *      ppom    ${config.users.users.ppom.home}/bin/check_co.sh''
+      ''* * * * *      ppom    ${check}''
     ];
   };
 }
