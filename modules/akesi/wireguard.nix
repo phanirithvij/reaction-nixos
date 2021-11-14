5cc8032f82191403697db8974f709e93154d1a49
ppom <ppom@ppom.me>
2021-11-14 23:42:37 +0100

    wireguard: fix wrong interface name

--- modules/akesi/wireguard.nix
+++ modules/akesi/wireguard.nix
@@ -3,23 +3,22 @@
 let
   wgPort = 123;
   genAddress = number: "10.10.0.${toString number}/32";
+  externalInterface = "ens3";
 in
 {
   # enable NAT
   networking.nat.enable = true;
-  networking.nat.externalInterface = "ens3";
+  networking.nat.externalInterface = externalInterface;
   networking.nat.internalInterfaces = [ "wg0" ];
 
   # Open WG port
   networking.firewall.allowedUDPPorts = [ wgPort ];
-
 
   # Enable routing
   boot.kernel.sysctl = {
     "net.ipv4.conf.all.forwarding" = lib.mkOverride 98 true;
     "net.ipv4.conf.default.forwarding" = lib.mkOverride 98 true;
   };
-
 
   networking.wireguard.interfaces = {
     wg0 = {
@@ -31,12 +30,12 @@ in
       # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
       # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
       postSetup = ''
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s ${genAddress 0} -o eth0 -j MASQUERADE
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s ${genAddress 0} -o ${externalInterface} -j MASQUERADE
       '';
 
       # This undoes the above command
       postShutdown = ''
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s ${genAddress 0} -o eth0 -j MASQUERADE
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s ${genAddress 0} -o ${externalInterface} -j MASQUERADE
       '';
 
       privateKeyFile = "/var/secrets/wireguard/privatekey";
