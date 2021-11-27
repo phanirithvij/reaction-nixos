0aedf41f2b567a868cf1ca03f948b8f4d8e8d968
ppom <ppom@ppom.me>
2021-11-27 10:05:42 +0100

    Fix video.ppom.me/sub bug
    When accessing /sub (no trailing slash),
    Links didn't have the sub prefix
    Example link: /Carol.2015 instead of /sub/Carol.2015

--- modules/musi/streama.nix
+++ modules/musi/streama.nix
@@ -24,12 +24,15 @@ in {
         };
 
         "/sub" = {
+          return = "301 /sub/";
+        };
+
+        "/sub/" = {
           # Needs the subsFilter NGINX module?
           root = "/data/streama/movies";
           extraConfig = ''
             # Remove the sub/ in the root dir
             rewrite ^/sub(/.*)$ $1 break;
-            rewrite ^/sub       /  break;
 
             # Show the files
             fancyindex on;
@@ -39,7 +42,7 @@ in {
             subs_filter '<tr>.*<a href="[^"]*.(mp4|mkv|avi)".*</tr>' ' ' r;
           '';
         };
-        "~ /sub.*\\.(mp4|mkv|avi)" = {
+        "~ /sub/.*\\.(mp4|mkv|avi)" = {
           return = "403";
         };
       };
