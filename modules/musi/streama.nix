7f451de1dab05e24c33235adc26b9d094b8dd294
ppom <ppom@ppom.me>
2022-02-17 10:12:31 +0100

    streama: Fix invalid redirection

--- modules/musi/streama.nix
+++ modules/musi/streama.nix
@@ -43,6 +43,7 @@ in {
             add_header X-XSS-Protection          "1; mode=block" always;
             # add_header Strict-Transport-Security "max-age=31536000";
             add_header Access-Control-Allow-Origin "https://video.ppom.me";
+            proxy_set_header X-Forwarded-Port $server_port;
           '';
         };
 
