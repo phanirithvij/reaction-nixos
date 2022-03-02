cb231c3772fb2d3f2ec9a62214709e26ae45bb09
ppom <ppom@ppom.me>
2022-03-02 12:49:36 +0100

    Add env variables to funkwhale AIO

--- modules/musi/funkwhale.nix
+++ modules/musi/funkwhale.nix
@@ -73,8 +73,11 @@ with lib;
           FUNKWHALE_API_IP = "127.0.0.1";
           FUNKWHALE_API_PORT = containerPort;
           FUNKWHALE_WEB_WORKERS = "4";
+          THROTTLING_RATES = "subsonic=5000/h";
           NESTED_PROXY = "1";
+          REVERSE_PROXY_TYPE = "nginx";
           NGINX_MAX_BODY_SIZE = cfg.maxBodySize;
+          MUSIC_DIRECTORY_SERVE_PATH = cfg.musicDir;
         };
         extraOptions = [
           # Only used to store the secret
