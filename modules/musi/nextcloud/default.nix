afa0480787f46e21dd91dc1c21408c040bf376a3
ppom <ppom@ppom.me>
2022-04-18 02:49:48 +0200

    Works on fail2ban

--- modules/musi/nextcloud/default.nix
+++ modules/musi/nextcloud/default.nix
@@ -99,9 +99,9 @@ in {
       port = 80,443
 
       filter = nextcloud
-      # Due to systemd backend as a default, we have to set this as pyinotify
+      # Due to systemd backend as a default, we have to set this as polling
       # (auto doesn't work when systemd is default backend)
-      backend = pyinotify
+      backend = polling
       logpath = /data/nextcloud/html/data/nextcloud.log
 
       maxretry = 3
