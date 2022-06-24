b50e7b868f8c99ccf15f69b270b6fc1a60dd9035
ppom <ppom@ppom.me>
2022-06-25 01:44:59 +0200

    Add back small.jpg conversion just in case

--- modules/akesi/pompeani.art.ci.sh
+++ modules/akesi/pompeani.art.ci.sh
@@ -31,7 +31,7 @@ fd big.jpg -x bash -c 'test -e "{//}/big.webp"   && test $(stat -c %Y -- "{}") -
 # shellcheck disable=SC2016
 fd big.jpg -x bash -c 'test -e "{//}/small.webp" && test $(stat -c %Y -- "{}") -lt $(stat -c %Y "{//}/small.webp") || convert -resize "600x1200>" "{}" "{//}/small.webp"'
 # shellcheck disable=SC2016
-# fd big.jpg -x bash -c 'test -e "{//}/small.jpg"  && test $(stat -c %Y -- "{}") -lt $(stat -c %Y "{//}/small.jpg")  || convert -resize "600x1200>" "{}" "{//}/small.jpg"'
+fd big.jpg -x bash -c 'test -e "{//}/small.jpg"  && test $(stat -c %Y -- "{}") -lt $(stat -c %Y "{//}/small.jpg")  || convert -resize "600x1200>" "{}" "{//}/small.jpg"'
 
 sed -i 's#^base_url.*#base_url = "'"$BASE_URL"'"#' config.toml
 
