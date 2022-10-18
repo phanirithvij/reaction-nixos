7baac00dc4e468c1c72a3a0f6761e7e34b2aff6c
ppom <ppom@ppom.me>
2022-10-18 17:34:40 +0200

    [pkgs] add mattermost-github-integration

---
+++ pkgs/mattermost-github-integration/default.nix
@@ -0,0 +1,36 @@
+{ lib, stdenv, fetchFromGitHub, python3 }:
+let 
+  pp = python3.withPackages(ps: with ps; [ requests pillow gunicorn flask ]);
+in stdenv.mkDerivation {
+  pname = "mattermost-github-integration";
+  version = "unstable-2022-01-13";
+
+  src = fetchFromGitHub {
+    owner = "softdevteam";
+    repo = "mattermost-github-integration";
+    rev = "c5ac79b5412992c227c7f61d87041fa9eb94e45f";
+    sha256 = "sha256-uE1Kj6CEMLL6II8580Yfu0Kag7e6d9iABbE8yRXzTEM=";
+  };
+
+  installPhase = ''
+    mkdir $out $out/bin
+    cp -r $src $out/src
+    chmod +w $out/src $out/src/mattermostgithub
+    ln -s /etc/mattermostgithub/config.py $out/src/mattermostgithub/config.py
+    # touch $out/src/mattermostgithub/config.py
+    cat > $out/bin/mattermostgithub <<EOF
+
+    ${pp}/bin/python $out/src/server.py
+
+    EOF
+    chmod +x $out/bin/mattermostgithub
+  '';
+
+  meta = with lib; {
+    description = "GitHub integration for Mattermost";
+    homepage = "https://github.com/softdevteam/mattermost-github-integration";
+    license = licenses.mit;
+    maintainers = with maintainers; [ ppom ];
+    platforms = platforms.linux ++ platforms.darwin;
+  };
+}
