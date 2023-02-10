9bca8e306c6ac1b40c9e5fc85d3d9fe6f8fd6f3c
ppom <ppom@ppom.me>
2023-02-10 19:20:26 +0100

    [musi/onlyoffice] give up onlyoffice for the moment.

--- modules/musi/file.ppom.me.nix
+++ modules/musi/file.ppom.me.nix
@@ -1,19 +1,5 @@
 { lib, pkgs, config, ... }:
 let
-  cfg = {
-    enable = true;
-    hostname = "onlyoffice.ppom.me";
-    jwtSecretFile = "/var/secrets/onlyoffice-jwt";
-    # jwtSecretFile = null;
-    # default values
-    # port = 8000;
-    # postgresHost = "/run/postgresql";
-    # postgresName = "onlyoffice";
-    # postgresUser = "onlyoffice";
-    # postgresPasswordFile = null;
-    # package = pkgs.onlyoffice-documentserver;
-    # rabbitmqUrl = "amqp://guest:guest@localhost:5672";
-  };
 in {
   services.nextcloud = {
     enable = true;
@@ -39,64 +25,7 @@ in {
     };
   };
 
-  services.onlyoffice = cfg;
-
-  # Required for onlyoffice
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "corefonts" ];
-
-  # systemd.services.onlyoffice-docservice.serviceConfig.ExecStartPre = lib.mkForce [
-  #   (pkgs.writeShellScript "onlyoffice-prestart" ''
-  #     PATH=$PATH:${lib.makeBinPath (with pkgs; [ jq moreutils config.services.postgresql.package ])}
-  #     umask 077
-  #     mkdir -p /run/onlyoffice/config/ /var/lib/onlyoffice/documentserver/sdkjs/{slide/themes,common}/ /var/lib/onlyoffice/documentserver/{fonts,server/FileConverter/bin}/
-  #     cp -r ${cfg.package}/etc/onlyoffice/documentserver/* /run/onlyoffice/config/
-  #     chmod u+w /run/onlyoffice/config/default.json
-
-  #     cp /run/onlyoffice/config/default.json{,.orig}
-
-  #     # for a mapping of environment variables from the docker container to json options see
-  #     # https://github.com/ONLYOFFICE/Docker-DocumentServer/blob/master/run-document-server.sh
-  #     jq '
-  #       .services.CoAuthoring.server.port = ${toString cfg.port} |
-  #       .services.CoAuthoring.sql.dbHost = "${cfg.postgresHost}" |
-  #       .services.CoAuthoring.sql.dbName = "${cfg.postgresName}" |
-  #     ${lib.optionalString (cfg.postgresPasswordFile != null) ''
-  #       .services.CoAuthoring.sql.dbPass = "'"$(cat ${cfg.postgresPasswordFile})"'" |
-  #     ''}
-  #       .services.CoAuthoring.sql.dbUser = "${cfg.postgresUser}" |
-  #     ${lib.optionalString (cfg.jwtSecretFile != null) ''
-  #       .services.CoAuthoring.token.enable.browser = true |
-  #       .services.CoAuthoring.token.enable.request.inbox = true |
-  #       .services.CoAuthoring.token.enable.request.outbox = true |
-  #       .services.CoAuthoring.secret.inbox.string = "'"$(cat ${cfg.jwtSecretFile})"'" |
-  #       .services.CoAuthoring.secret.outbox.string = "'"$(cat ${cfg.jwtSecretFile})"'" |
-  #       .services.CoAuthoring.secret.session.string = "'"$(cat ${cfg.jwtSecretFile})"'" |
-  #     ''}
-  #       .rabbitmq.url = "${cfg.rabbitmqUrl}"
-  #       ' /run/onlyoffice/config/default.json | sponge /run/onlyoffice/config/default.json
-
-  #     if psql -d onlyoffice -c "SELECT 'task_result'::regclass;" >/dev/null; then
-  #       psql -f ${cfg.package}/var/www/onlyoffice/documentserver/server/schema/postgresql/removetbl.sql
-  #       psql -f ${cfg.package}/var/www/onlyoffice/documentserver/server/schema/postgresql/createdb.sql
-  #     else
-  #       psql -f ${cfg.package}/var/www/onlyoffice/documentserver/server/schema/postgresql/createdb.sql
-  #     fi
-  #   '')
-  #   (pkgs.writeShellScript "onlyoffice-custom-settings" ''
-  #     ${pkgs.jq}/bin/jq '
-  #       .wopi.enable = true |
-  #       .wopi.host = "file.ppom.me"
-  #     ' /run/onlyoffice/config/default.json | ${pkgs.moreutils}/bin/sponge /run/onlyoffice/config/default.json
-  #   '')
-  # ];
-
-  # users.users.nextcloud.extraGroups = [ "postmaster" ];
-
   services.nginx.virtualHosts = {
-    "onlyoffice.ppom.me" = {
-      enableACME = true;
-      forceSSL = true;
-    };
     "file.ppom.me" = {
       enableACME = true;
       forceSSL = true;
