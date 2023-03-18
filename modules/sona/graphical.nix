5edab7f0716ff036582ad4c2cebe55be05f8cf26
ppom <sona@ppom.me>
2023-03-18 21:43:39 +0100

    [sona/steam] make steam be here again
    Seems not very stable on Wayland but it's fiiine

--- modules/sona/graphical.nix
+++ modules/sona/graphical.nix
@@ -97,6 +97,8 @@ in
 
     rbw-wofi
     passwofi
+
+    steam
   ];
 
   nixpkgs.overlays = [
@@ -140,17 +142,12 @@ in
     wrapperFeatures.gtk = true;
   };
 
-  ### BEGIN UNFREE
-  # Yeah, I'm not proud of that
-  # nixpkgs.config.allowUnfree = true;
-  # nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "steam" "nvidia" ];
-
   # Steam related
-  # environment.systemPackages = [ pkgs.steam ];
-  # hardware.opengl.driSupport32Bit = true;
-  # hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
-  # hardware.pulseaudio.support32Bit = true;
-  ### END UNFREE
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "steam"  "steam-original" ];
+  hardware.opengl = {
+    driSupport32Bit = true;
+    extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+  };
 
   # Fix of: Can't shutdown after having suspended the laptop by closing it.
   # Fix found here: https://bugs.launchpad.net/ubuntu/+source/systemd/+bug/1441253
