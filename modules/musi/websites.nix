524686e1cf36aab13eaf85f56d501ae856df48f6
ppom <musi@ppom.me>
2023-04-23 15:23:18 +0200

    [musi/slskd] allow unsafe-inline in CSP to fix webui

--- modules/musi/websites.nix
+++ modules/musi/websites.nix
@@ -64,7 +64,7 @@ in {
           # tell browsers to only send https://domain.name as Referer
           add_header Referrer-Policy "strict-origin";
           # CSP
-          add_header Content-Security-Policy "default-src 'self'; style-src 'self' 'unsafe-inline'; frame-ancestors: 'none';";
+          add_header Content-Security-Policy "default-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; frame-ancestors: 'none';";
         '';
       };
 
