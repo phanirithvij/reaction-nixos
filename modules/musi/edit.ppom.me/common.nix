8c2763b899219d36138c4e2a17372e874f669208
ppom <musi@ppom.me>
2023-06-05 15:25:51 +0200

    [directus] fix directus refusing to connect to localhost
    Non announced breaking change from directus :sob:
    https://github.com/directus/directus/issues/18812

--- modules/musi/edit.ppom.me/common.nix
+++ modules/musi/edit.ppom.me/common.nix
@@ -10,6 +10,9 @@
     EMAIL_SMTP_PASSWORD_FILE = "/var/secrets/mail/postmaster"; # Must not end with a newline
 
     LOG_LEVEL = "warn";
+
+    # Defaults to denying localhost
+    IMPORT_IP_DENY_LIST = "";
   };
 
   sshKey = "/var/secrets/pompeani.art/key";
