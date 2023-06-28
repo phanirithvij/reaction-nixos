1d7aecff55eecd3eae24c08fa3929917dd13b610
ppom <musi@ppom.me>
2023-06-28 15:20:01 +0200

    [slskd] don't need logs; they're already stored by journald

--- modules/common/slskd.nix
+++ modules/common/slskd.nix
@@ -196,8 +196,7 @@ in {
         Type = "oneshot";
         User = "slskd";
         ExecStart = [
-          "${pkgs.findutils}/bin/find /var/lib/slskd/logs/ -type f -mtime +10 -delete"
-          "${pkgs.findutils}/bin/find /var/lib/slskd/logs/ -type f -mtime +1  -exec ${pkgs.gzip}/bin/gzip -q {} \\;"
+          "${pkgs.findutils}/bin/find /var/lib/slskd/logs/ -type f -delete"
         ];
       };
       startAt = "daily";
