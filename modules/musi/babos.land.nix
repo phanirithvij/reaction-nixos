9c6342a133b4e72db70679c075a57b2c93063cc8
ppom <musi@ppom.me>
2023-07-09 16:46:04 +0200

    [musi/babos.land] add directus instance

--- modules/musi/babos.land.nix
+++ modules/musi/babos.land.nix
@@ -22,4 +22,19 @@ in {
       "kiosque.babos.land" = generalConf;
       "tribune.babos.land" = generalConf;
   };
+
+  services.directus.servers."babos" = {
+    enable = true;
+    settings = (import ./edit.ppom.me/common.nix {}).settings // {
+      EMAIL_FROM = "babos.land@ppom.me";
+      PORT = 8058;
+    };
+    nginx = {
+      enable = true;
+      serverName = "babos.land";
+      location = "/repertoire";
+    };
+  };
+
+  users.users."directus-babos".extraGroups = [ "postmaster" ];
 }
