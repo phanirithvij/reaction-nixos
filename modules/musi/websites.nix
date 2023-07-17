ad3e7d02139e49e1370a6bc8b00b09c5598a6118
ppom <musi@ppom.me>
2023-07-18 01:25:42 +0200

    [musi/u.ppom.me] more flexible CSP

--- modules/musi/websites.nix
+++ modules/musi/websites.nix
@@ -104,7 +104,7 @@ in {
         };
         extraConfig = ''
           # add_header Strict-Transport-Security "max-age=31536000";
-          add_header Content-Security-Policy "default-src 'self'; frame-ancestors 'none'; style-src 'self' 'unsafe-inline'";
+          add_header Content-Security-Policy "default-src 'self' 'unsafe-inline'; frame-ancestors 'none'";
           add_header X-Content-Type-Options "nosniff";
           add_header X-Frame-Options "DENY";
         '';
