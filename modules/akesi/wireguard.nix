707959e1092365f4e317c5e391c67219b6e1799e
ppom <akesi@ppom.me>
2023-07-18 21:06:39 +0200

    [akesi/musi] untested, disabled wireguard+nfs setup

--- modules/akesi/wireguard.nix
+++ modules/akesi/wireguard.nix
@@ -1,83 +1,29 @@
 { lib, config, pkgs, ... }:
-
-let
-  wgPort = 123;
-  externalInterface = "ens3";
-  wireguardInterface = "wg0";
-  blocklistPath = "/var/cache/dnsmasq.blacklist.txt";
-  blocklistUrl = "https://raw.githubusercontent.com/notracking/hosts-blocklists/master/dnsmasq/dnsmasq.blacklist.txt";
-in
 {
-  # enable NAT
-  networking.nat = {
-    enable = true;
-    externalInterface = externalInterface;
-    internalInterfaces = [ wireguardInterface ];
-  };
+  environment.systemPackages = [ pkgs.wireguard-tools ];
+  networking.firewall.allowedUDPPorts = [ 8095 ];
 
-  # Open WG port
-  networking.firewall = {
-    allowedTCPPorts = [ 53 wgPort ];
-    allowedUDPPorts = [ 53 wgPort ];
-    trustedInterfaces = [ wireguardInterface ];
-  };
-  # Enable routing
-  boot.kernel.sysctl = {
-    "net.ipv4.conf.all.forwarding" = lib.mkOverride 98 true;
-    "net.ipv4.conf.default.forwarding" = lib.mkOverride 98 true;
-  };
+  # networking.wg-quick.interfaces.wg0 = {
+  #   address = [ "10.0.0.1/24" ];
+  #   listenPort = 8095;
+  #   privateKeyFile = "/var/secrets/wg0";
+  #   peers = [
+  #     # musi
+  #     {
+  #       publicKey = "xN+0miJ1+iSvhEgYcymMJsZjduRuPYsZqbHnW8YBUTE=";
+  #       allowedIPs = [ "10.0.0.0/24" ];
+  #       persistentKeepalive = 25;
+  #     }
+  #   ];
+  # };
 
-  services.dnsmasq = {
-    enable = true;
-    extraConfig = ''
-      interface=${wireguardInterface}
-
-      domain-needed # Won't forward request for "foo", only "foo.bar"
-      bogus-priv # Won't forward requests for local IP adresses
-      conf-file=${blocklistPath} # Block trackers
-
-      # log-queries
-    '';
-  };
-  environment.systemPackages = [ pkgs.dnsmasq pkgs.tcpdump ];
-
-  systemd.services.fetch_blocklist = {
-    description = "update DNSmasq's blocklist";
-    script = "${pkgs.curl}/bin/curl ${blocklistUrl} -o ${blocklistPath}";
-    startAt = "daily";
-  };
-
-  networking.wg-quick.interfaces = {
-    "${wireguardInterface}" = {
-      # Determines the IP address and subnet of the server's end of the tunnel interface.
-      address = [ "10.0.0.1/24" ];
-
-      listenPort = wgPort;
-
-      # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
-      postUp = ''
-        ${pkgs.iptables}/bin/iptables -A FORWARD -i ${wireguardInterface} -j ACCEPT
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.0.0.0/16 -o ${externalInterface} -j MASQUERADE
-      '';
-
-      # Undo the above
-      preDown = ''
-        ${pkgs.iptables}/bin/iptables -D FORWARD -i ${wireguardInterface} -j ACCEPT
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.0/16 -o ${externalInterface} -j MASQUERADE
-      '';
-
-      privateKeyFile = "/var/secrets/wireguard/privatekey";
-
-      peers = [
-        { # sona
-          publicKey = "UYjsvFMCc+yRBPxX4rHiuRx1jQd1WntClaAueNXNmh4=";
-          allowedIPs = [ "10.0.0.2/32" ];
-        }
-        { # ilo
-          publicKey = "Zz7tZ6UlbiCokqMI5BFTBiqn48taHL633ruWR6wi5GY=";
-          allowedIPs = [ "10.0.0.3/32" ];
-        }
-      ];
-    };
-  };
+  # fileSystems."/musi" = {
+  #   device = "10.0.0.2:/data/user-uploads/akesi";
+  #   options = [
+  #     # Lazy mounting
+  #     "x-systemd.automount" "noauto"
+  #     # v3
+  #     "nfsvers=3"
+  #   ];
+  # };
 }
