6b60b385b791453e128252c3e13b46ed24d23568
ppom <sona@ppom.me>
2023-09-14 17:55:01 +0200

    [pkgs/fluffychat-web] use nixpkgs' build version with overrideAttrs

--- pkgs/fluffychat-web/default.nix
+++ pkgs/fluffychat-web/default.nix
@@ -1,35 +1,22 @@
 { lib
-, callPackage
-, llvmPackages_13
 , flutter
-, jq
 , fetchFromGitHub
-, imagemagick
-, mesa
-, libdrm
-, pulseaudio
-, makeDesktopItem
-, gnome
-, olm
+#, olm
 }:
 
 let
-  libwebrtcRpath = lib.makeLibraryPath [ mesa libdrm ];
-  olm' = olm.overrideAttrs(old: {
-    version = "fork-unstable-2022-01-31";
-    src = fetchFromGitHub {
-      owner = "famedly";
-      repo = "olm";
-      rev = "580bc452433aa422d649b3969f56efa2adf1a8a5";
-      sha256 = "sha256-TTY0LH1jcRSuNsqCST/Rfe/W1pL8lOEyMLC2aWHqNJI=";
-    };
-  });
+  # olm' = olm.overrideAttrs(old: {
+  #   version = "fork-unstable-2022-01-31";
+  #   src = fetchFromGitHub {
+  #     owner = "famedly";
+  #     repo = "olm";
+  #     rev = "580bc452433aa422d649b3969f56efa2adf1a8a5";
+  #     sha256 = "sha256-TTY0LH1jcRSuNsqCST/Rfe/W1pL8lOEyMLC2aWHqNJI=";
+  #   };
+  # });
   version = "1.13.0";
 
-# We heavily change how we build the flutter application
-# since we're not building for linux but for the web.
-# TODO try to upstream this
-in (import ./flutter.nix { inherit lib callPackage llvmPackages_13 flutter jq; }) rec {
+in (flutter.buildFlutterApplication {
   pname = "fluffychat-web";
   inherit version;
 
@@ -43,10 +30,6 @@ in (import ./flutter.nix { inherit lib callPackage llvmPackages_13 flutter jq; }
   depsListFile = ./deps.json;
   vendorHash = "sha256-Ot96+EF8PgYQmXn0hvIWzN8StuzTgQzakRO3yf7PJAU=";
 
-  nativeBuildInputs = [ imagemagick ];
-
-  env.NIX_LDFLAGS = "-rpath-link ${libwebrtcRpath}";
-
   meta = with lib; {
     description = "Chat with your friends (matrix web client)";
     homepage = "https://fluffychat.im/";
@@ -55,4 +38,25 @@ in (import ./flutter.nix { inherit lib callPackage llvmPackages_13 flutter jq; }
     platforms = [ "x86_64-linux" "aarch64-linux" ];
     sourceProvenance = [ sourceTypes.fromSource ];
   };
-}
+}).overrideAttrs (oldAttrs: {
+  outputs = [ "out" ];
+
+  buildPhase = ''
+      runHook preBuild
+
+      doPubGet flutter pub get --offline -v
+      flutter build web -v --release
+
+      runHook postBuild
+  '';
+
+  installPhase = ''
+      runHook preInstall
+
+      mv build/web $out
+
+      runHook postInstall
+  '';
+
+  postFixup = "";
+})
