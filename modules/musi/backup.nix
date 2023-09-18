79ec90c1cd9ecde3215bf747af9ff644f9658486
ppom <musi@ppom.me>
2023-09-18 17:17:30 +0200

    [musi/backup] switch from borg to restic

--- modules/musi/backup.nix
+++ modules/musi/backup.nix
@@ -1,29 +1,7 @@
 { lib, pkgs, config, ... }:
 with lib;
 let
-  backup = { name, paths, startAt ? "daily", extraArgs ? {} }: {
-    "${name}" = {
-        paths = paths;
-        startAt = startAt;
-        prune.keep = {
-          within = "1d"; # Keep all archives from the last day
-          daily = 7;
-          weekly = 4;
-          monthly = -1;  # Keep at least one archive for each month
-        };
-        encryption = {
-          mode = "repokey-blake2";
-          passCommand = "cat /var/secrets/backups/${name}/pass";
-        };
-        compression = "lz4";
-        environment = {
-          BORG_RSH = "ssh -i /var/secrets/backups/${name}/sshkey";
-        };
-        repo = "ppom@node.cmercier.fr:/pacobackup/${name}";
-    } // extraArgs;
-  };
-in
-{
+in {
   services.postgresqlBackup = {
     enable = true;
     # every 6 hours, it only costs 2s of CPU time for now
@@ -36,11 +14,26 @@ in
     calendar = "*-*-* 0/6:15";
   };
 
-  services.borgbackup = {
-    jobs = backup {
-      name = "data";
-      paths = [ "/data/" "/var/" "/etc/nixos/" "/home/" "/root/" "/nix/var/nix/" ];
-      startAt = [ "*-*-* 02:00" ];
+  services.restic.backups.data = {
+    paths = [ "/data/" "/var/" "/etc/nixos/" "/home/" "/root/" "/nix/var/nix/" ];
+    passwordFile = "/var/secrets/backups/data/pass";
+    extraOptions = [
+      "sftp.command='ssh ppom@node.cmercier.fr -i /var/secrets/backups/data/sshkey -s sftp'"
+    ];
+    repository = "sftp:ppom@node.cmercier.fr:/pacobackup/data";
+    initialize = true;
+    pruneOpts = [
+      "--keep-daily 7"
+      "--keep-weekly 5"
+      "--keep-monthly 12"
+    ];
+    exclude = [
+      "/var/cache"
+      "/home/*/.cache"
+    ];
+    timerConfig = {
+      OnCalendar = [ "02:00" ];
+      RandomizedDelaySec = "30m";
     };
   };
 }
