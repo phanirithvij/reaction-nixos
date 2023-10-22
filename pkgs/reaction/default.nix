b0069bbb38ed3452fddfed3cfbb2847acf871e94
ppom <sona@ppom.me>
2023-10-22 12:00:00 +0200

    [pkgs/reaction] bump to v0.4

--- pkgs/reaction/default.nix
+++ pkgs/reaction/default.nix
@@ -1,17 +1,35 @@
-{ buildGoModule, fetchFromGitLab }:
+{
+stdenv
+, linkFarm
+, buildGoModule
+, fetchFromGitLab
+}:
 let
   pname = "reaction";
-  version = "v0.2";
-in buildGoModule {
-  inherit pname version;
-
+  version = "v0.4";
   src = fetchFromGitLab {
     domain = "framagit.org";
     owner = "ppom";
     repo = pname;
     rev = version;
-    sha256 = "sha256-Onj/KdJwe0ulEGjiYA4GyKKZg6OA8UXujOQqHeHDJ+U=";
+    sha256 = "sha256-sRum8r+zFCF0k1EFkGFOR2EqSE6O/B5xiU7nJP518YQ=";
   };
+  reaction = buildGoModule {
+    inherit pname version src;
 
-  vendorHash = "sha256-g+yaVIx4jxpAQ/+WrGKxhVeliYx7nLQe/zsGpxV4Fn4=";
-}
+    vendorHash = "sha256-THUIoWFzkqaTofwH4clBgsmtUlLS9WIB2xjqW7vkhpg=";
+  };
+  ip46tables = stdenv.mkDerivation {
+    inherit pname version src;
+    buildPhase = ''
+      make ip46tables
+    '';
+    installPhase = ''
+      mkdir -p $out/bin
+      cp ip46tables $out/bin
+    '';
+  };
+in linkFarm "reaction" [
+  { name = "bin/reaction"; path = "${reaction}/bin/reaction"; }
+  { name = "bin/ip46tables"; path = "${ip46tables}/bin/ip46tables"; }
+]
