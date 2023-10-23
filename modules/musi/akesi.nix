1d909d7db0fe747e59c5a26144cf91982684393c
ppom <musi@ppom.me>
2023-10-23 12:00:00 +0200

    [musi-akesi] nebula: fail

--- modules/musi/akesi.nix
+++ modules/musi/akesi.nix
@@ -1,45 +1,33 @@
 { config, pkgs, ... }:
 {
-  environment.systemPackages = [ pkgs.wireguard-tools ];
+  # services.nfs.server = {
+  #   enable = true;
+  #   exports = ''
+  #     /data/user-uploads/akesi 192.168.100.1(rw,nohide,insecure,no_subtree_check)
+  #   '';
+  # };
 
-  networking.wg-quick.interfaces.wg0 = {
-    address = [ "10.0.0.2/24" ];
-    privateKeyFile = "/var/secrets/wg0";
-    peers = [
-      # akesi
-      {
-        publicKey = "fFH+oRg+iU2tKzCksRvr+JYdAf3CAd8MgSwkY/2ctgE=";
-        allowedIPs = [ "10.0.0.0/24" ];
-        endpoint = "54.37.74.51:8095";
-        persistentKeepalive = 25;
-      }
-    ];
-  };
-
-  services.nfs.server = {
-    enable = true;
-    exports = ''
-      /data/user-uploads/akesi 10.0.0.1(rw,nohide,insecure,no_subtree_check)
-    '';
-  };
-
-  networking.firewall.allowedUDPPorts = [ 4242 ];
-  services.nebula.networks.kulupu = {
-    enable = true;
-    ca   = "/var/secrets/nebula/kulupu/ca.crt";
-    cert = "/var/secrets/nebula/kulupu/musi.crt";
-    key  = "/var/secrets/nebula/kulupu/musi.key";
-    firewall = {
-      outbound = [
-        { host = "any"; port = "any"; proto = "icmp"; }
-      ];
-      inbound = [
-        { host = "musi"; port = "2049"; proto = "any"; }
-        { host = "any"; port = "any"; proto = "icmp"; }
-      ];
-    };
-    isLighthouse = true;
-    staticHostMap = { "192.168.100.1" = [ "54.37.74.51" ]; };
-    settings = {};
-  };
+  # networking.firewall.allowedUDPPorts = [ 4242 ];
+  # systemd.services."nebula@kulupu".serviceConfig.ReadOnlyPaths = [ "/var/secrets/nebula/kulupu/" ];
+  # services.nebula.networks.kulupu = {
+  #   enable = true;
+  #   ca   = "/var/secrets/nebula/kulupu/ca.crt";
+  #   cert = "/var/secrets/nebula/kulupu/musi.crt";
+  #   key  = "/var/secrets/nebula/kulupu/musi.key";
+  #   firewall = {
+  #     outbound = [
+  #       { host = "any"; port = "any"; proto = "any"; }
+  #     ];
+  #     inbound = [
+  #       { host = "any"; port = "any"; proto = "any"; }
+  #     ];
+  #   };
+  #   isLighthouse = true;
+  #   staticHostMap = { "192.168.100.1" = [ "54.37.74.51:4242" ]; };
+  #   settings = {
+  #     lighthouse = {
+  #       hosts = "192.168.100.1";
+  #     };
+  #   };
+  # };
 }
