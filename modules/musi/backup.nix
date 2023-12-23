f77b85d0d7211a5fe5951674d1a6f99a1d48fd3d
ppom <musi@ppom.me>
2023-12-23 12:00:00 +0100

    [musi] add poki transient backup config

--- modules/musi/backup.nix
+++ modules/musi/backup.nix
@@ -36,4 +36,27 @@ in {
       RandomizedDelaySec = "30m";
     };
   };
+
+  services.restic.backups.data2 = {
+    paths = [ "/data/" "/var/" "/etc/nixos/" "/home/" "/root/" "/nix/var/nix/" ];
+    passwordFile = "/var/secrets/backups/data2/pass";
+    extraOptions = [
+      "sftp.command='ssh musi@192.168.1.74 -i /var/secrets/backups/data2/sshkey -s sftp'"
+    ];
+    repository = "sftp:musi@192.168.1.74:/backup/musi/restic";
+    initialize = true;
+    pruneOpts = [
+      "--keep-daily 7"
+      "--keep-weekly 5"
+      "--keep-monthly 12"
+    ];
+    exclude = [
+      "/var/cache"
+      "/home/*/.cache"
+    ];
+    timerConfig = {
+      OnCalendar = [ "02:00" ];
+      RandomizedDelaySec = "30m";
+    };
+  };
 }
