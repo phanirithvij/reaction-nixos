5b33040a5bc82c247785d3a392396d57a4595947
ppom <sona@ppom.me>
2024-01-18 12:00:00 +0100

    [sona/pkgs] add prose plugin to tailwindcss

--- modules/sona/packages.nix
+++ modules/sona/packages.nix
@@ -95,7 +95,7 @@ in {
     h2 # H2 Database Editor
     # alejandra # Nix formatter
     zola # static site generator
-    nodePackages.tailwindcss # CSS generation framework
+    (nodePackages.tailwindcss.overrideAttrs (old: { plugins = [ nodePackages."@tailwindcss/typography" ]; })) # CSS generation framework
     # gcc-wrapper
     sqlitebrowser
     mmctl # mattermost control (for Picasoft's server management)
