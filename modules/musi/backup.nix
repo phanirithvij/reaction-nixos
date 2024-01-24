7df44cad5d7349875e280ed9f78ba7eb1971b0d4
ppom <musi@ppom.me>
2024-01-24 12:00:00 +0100

    [musi/backup] now waking-on-lan backup target

--- modules/musi/backup.nix
+++ modules/musi/backup.nix
@@ -37,13 +37,16 @@ in {
     };
   };
 
-  services.restic.backups.data2 = {
+  services.restic.backups.data2 = let
+    pokiHost = "musi@192.168.1.74";
+    pokiKey = "/var/secrets/backups/data2/sshkey";
+  in {
     paths = [ "/data/" "/var/" "/etc/nixos/" "/home/" "/root/" "/nix/var/nix/" ];
     passwordFile = "/var/secrets/backups/data2/pass";
     extraOptions = [
-      "sftp.command='ssh musi@192.168.1.74 -i /var/secrets/backups/data2/sshkey -s sftp'"
+      "sftp.command='ssh ${pokiHost} -i ${pokiKey} -s sftp'"
     ];
-    repository = "sftp:musi@192.168.1.74:/backup/musi/restic";
+    repository = "sftp:${pokiHost}:/backup/musi/restic";
     initialize = true;
     pruneOpts = [
       "--keep-daily 7"
@@ -58,5 +61,13 @@ in {
       OnCalendar = [ "04:00" ];
       RandomizedDelaySec = "30m";
     };
+    backupPrepareCommand = "${pkgs.writeScript "wake-poki" ''
+      ${pkgs.wakelan}/bin/wakelan A0:B3:CC:E9:4C:9C
+      for _ in $(seq 60)
+      do
+        sleep 5
+        ssh ${pokiHost} -i ${pokiKey} -o ConnectTimeout=10
+      done
+    ''}";
   };
 }
