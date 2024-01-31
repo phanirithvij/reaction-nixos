49d257954eb82e5248c449ef9e0d77c15a4165bd
ppom <sona@ppom.me>
2024-01-31 12:00:00 +0100

    [common/nvim] fix Far's rg source engine

--- modules/common/init.lua
+++ modules/common/init.lua
@@ -1,42 +1,42 @@
 local options = {
-  number = true,
-  termguicolors = true,
-  -- Save changes in swapfile
-  updatetime = 100,
-  -- Open new split panes to right and below
-  splitright = true,
-  splitbelow = true,
-  -- Terminal Title
-  title = true,
-  -- Completion look
-  completeopt = "noinsert,menuone,noselect",
+	number = true,
+	termguicolors = true,
+	-- Save changes in swapfile
+	updatetime = 100,
+	-- Open new split panes to right and below
+	splitright = true,
+	splitbelow = true,
+	-- Terminal Title
+	title = true,
+	-- Completion look
+	completeopt = "noinsert,menuone,noselect",
 
-  -- Keep n lines between the cursor and the edge of the screen
-  scrolloff = 3,
+	-- Keep n lines between the cursor and the edge of the screen
+	scrolloff = 3,
 
-  -- Make macros complete faster
-  lazyredraw = true,
+	-- Make macros complete faster
+	lazyredraw = true,
 
-  -- Insensitive search
-  smartcase = true,
-  -- Insensitive search in command mode for files and directories
-  wildignorecase = true,
-  ignorecase = true,
+	-- Insensitive search
+	smartcase = true,
+	-- Insensitive search in command mode for files and directories
+	wildignorecase = true,
+	ignorecase = true,
 
-  -- Show in real time what :s will do
-  inccommand = "nosplit",
-  -- Show the match when closing brackets
-  showmatch = true,
+	-- Show in real time what :s will do
+	inccommand = "nosplit",
+	-- Show the match when closing brackets
+	showmatch = true,
 
-  -- Do not break words
-  linebreak = true,
+	-- Do not break words
+	linebreak = true,
 
-  -- PaperColor/gruvbox light theme
-  -- background = "light"
+	-- PaperColor/gruvbox light theme
+	-- background = "light"
 }
 
 for k, v in pairs(options) do
-  vim.o[k] = v
+	vim.o[k] = v
 end
 
 vim.cmd.colorscheme "gruvbox"
@@ -55,15 +55,15 @@ vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 
 -- Far
-vim.g["far#source"] = "rgnvim"
+vim.g["far#source"] = "rg"
 vim.g["far#glob_mode"] = "native"
 
 vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    -- silent!
-    vim.highlight.on_yank { on_visual = false }
-  end,
-  group = vim.api.nvim_create_augroup('yank_conf', { clear = true })
+	callback = function()
+		-- silent!
+		vim.highlight.on_yank { on_visual = false }
+	end,
+	group = vim.api.nvim_create_augroup('yank_conf', { clear = true })
 })
 
 vim.api.nvim_create_user_command("RemoveTrailingSpaces", [[%s/\s\+$//]], { force = true })
@@ -74,24 +74,24 @@ vim.api.nvim_create_user_command("TrimDots", [[%s/\.\././g]], { force = true })
 -- Terminal Configuration
 vim.keymap.set('t', '²', [[<C-\><C-n>]], {})
 vim.api.nvim_create_autocmd('TermOpen', {
-  callback = function()
-    vim.bo.nonu = true
-    vim.cmd.startinsert()
-  end,
-  group = vim.api.nvim_create_augroup('terminal_conf', { clear = true })
+	callback = function()
+		vim.bo.nonu = true
+		vim.cmd.startinsert()
+	end,
+	group = vim.api.nvim_create_augroup('terminal_conf', { clear = true })
 })
 
 -- map ù to /log what's under the cursor/
 local stdout = vim.api.nvim_create_augroup('stdout', { clear = true })
 local stdoutautocmd = function(filetype, text)
-  vim.api.nvim_create_autocmd('Filetype', {
-    pattern = filetype,
-    callback = function()
-      vim.keymap.set('n', 'ù', 'yiwo' .. text .. '<esc>')
-      vim.keymap.set('v', 'ù', 'yo' .. text .. '<esc>')
-    end,
-    group = stdout
-  })
+	vim.api.nvim_create_autocmd('Filetype', {
+		pattern = filetype,
+		callback = function()
+			vim.keymap.set('n', 'ù', 'yiwo' .. text .. '<esc>')
+			vim.keymap.set('v', 'ù', 'yo' .. text .. '<esc>')
+		end,
+		group = stdout
+	})
 end
 stdoutautocmd('python', 'print(f"<Esc>pa: {<Esc>pa}")')
 stdoutautocmd('fish', 'echo <Esc>pa: $<Esc>p')
@@ -106,56 +106,56 @@ stdoutautocmd('lua', 'print("<Esc>pa:", <Esc>pa)')
 -- LSP config
 
 if enableNixd then
-  local lspconfig = require 'lspconfig'
-  lspconfig.nixd.setup {}
+	local lspconfig = require 'lspconfig'
+	lspconfig.nixd.setup {}
 end
 
 if steroids then
-  local lspconfig = require 'lspconfig'
-  vim.g.vim_markdown_folding_disabled = 1
-  vim.g.vim_markdown_toc_autofit = 1
+	local lspconfig = require 'lspconfig'
+	vim.g.vim_markdown_folding_disabled = 1
+	vim.g.vim_markdown_toc_autofit = 1
 
-  -- LSP config
-  lspconfig.gopls.setup {}
-  lspconfig.bashls.setup {}
-  -- lspconfig.ltex.setup {}
-  lspconfig.rust_analyzer.setup {}
-  lspconfig.jsonnet_ls.setup {}
+	-- LSP config
+	lspconfig.gopls.setup {}
+	lspconfig.bashls.setup {}
+	-- lspconfig.ltex.setup {}
+	lspconfig.rust_analyzer.setup {}
+	lspconfig.jsonnet_ls.setup {}
 
-  lspconfig.html.setup {}
-  lspconfig.jsonls.setup {}
-  lspconfig.cssls.setup {}
+	lspconfig.html.setup {}
+	lspconfig.jsonls.setup {}
+	lspconfig.cssls.setup {}
 
-  lspconfig.tailwindcss.setup {}
-  lspconfig.tsserver.setup {}
-  lspconfig.svelte.setup {}
+	lspconfig.tailwindcss.setup {}
+	lspconfig.tsserver.setup {}
+	lspconfig.svelte.setup {}
 
-  lspconfig.lua_ls.setup { settings = {
-    Lua = {
-      runtime = { version = 'LuaJIT' },
-      diagnostics = { globals = { 'vim' } }, -- recognize the `vim` global
-      workspace = {                          -- aware of Neovim runtime files (long)
-        library = vim.api.nvim_get_runtime_file("", true)
-      },
-      telemetry = { enable = false },
-    }
-  } }
+	lspconfig.lua_ls.setup { settings = {
+		Lua = {
+			runtime = { version = 'LuaJIT' },
+			diagnostics = { globals = { 'vim' } }, -- recognize the `vim` global
+			workspace = {     -- aware of Neovim runtime files (long)
+				library = vim.api.nvim_get_runtime_file("", true)
+			},
+			telemetry = { enable = false },
+		}
+	} }
 end
 
 
 local source_if_exists = function(filename)
-  local file_exists = function(filename)
-    local f = io.open(filename, "r")
-    if f ~= nil then
-      io.close(f)
-      return true
-    else
-      return false
-    end
-  end
-  local filename = vim.fs.normalize(filename)
-  if file_exists(filename) then
-    vim.cmd.source(filename)
-  end
+	local file_exists = function(filename)
+		local f = io.open(filename, "r")
+		if f ~= nil then
+			io.close(f)
+			return true
+		else
+			return false
+		end
+	end
+	local filename = vim.fs.normalize(filename)
+	if file_exists(filename) then
+		vim.cmd.source(filename)
+	end
 end
 source_if_exists "~/.config/nvim/init.lua"
