703b13f83803d6abe2e9143a9f20acb01d996f42
ppom <musi@ppom.me>
2024-02-29 12:00:00 +0100

    [musi/dyndns] directly editing the OVH DNS zone

--- modules/musi/dyndns.nix
+++ modules/musi/dyndns.nix
@@ -1,22 +1,17 @@
 {config, pkgs, ...}:
 {
-  systemd.services.dyndns = {
+  systemd.services.dyndns = let 
+    python = pkgs.python3.withPackages (p: [ p.ovh ]);
+    script = ./dyndns.py;
+  in {
     startAt = "*:0/10";
-    script = ''
-      set -e
-      TOKEN="$(cat /var/secrets/ydns-token)"
-      IPv6="$(${pkgs.iproute2}/bin/ip -j a | ${pkgs.jq}/bin/jq -r '[
-        .[]
-        | select(.ifname == "enp6s0")
-        | .addr_info[]
-        | select(.scope == "global")
-        | select(.family == "inet6")
-        | select(.deprecated == false or has("deprecated") | not)
-      ]
-      | max_by(.preferred_life_time)
-      | .local')"
-      ${pkgs.curl}/bin/curl -sS "https://ydns.io/hosts/update/$TOKEN?ip=$IPV6"
-    '';
+    serviceConfig = {
+      EnvironmentFile = "/var/secrets/ovh-token";
+      Environment = [
+        "IP=${pkgs.iproute2}/bin/ip"
+        "IFNAME=enp6s0"
+      ];
+      ExecStart = "${python}/bin/python ${script}";
+    };
   };
-
 }
---
+++ modules/musi/dyndns.py
@@ -0,0 +1,65 @@
+# Shebang the following lines to get self-wrapped script
+#/usr/bin/env nix-shell
+# nix-shell -i python -p python3Packages.ovh
+
+# Environment variables
+# see https://github.com/ovh/python-ovh#environment-vars-and-predefined-configuration-files
+
+# IP: /path/to/ip executable
+# IFNAME: interface name
+# OVH_ENDPOINT
+# OVH_APPLICATION_KEY
+# OVH_APPLICATION_SECRET
+# OVH_CONSUMER_KEY
+
+import json
+import os
+import subprocess
+import sys
+
+import ovh
+
+def getIp():
+    ip = os.environ['IP']
+    res = subprocess.run([ip, "--json", "address"], capture_output=True, text=True)
+    data = json.loads(res.stdout)
+
+    ifname = os.environ['IFNAME']
+    addresses = [
+        interface['addr_info']
+        for interface in data
+        if interface['ifname'] == ifname
+    ][0]
+
+    ipv6 = max([
+            address
+            for address in addresses
+            if address['scope'] == "global"
+            and address['family'] == "inet6"
+            and not ('deprecated' in address and address['deprecated'])
+        ],
+        key=lambda a: a['preferred_life_time']
+    )['local']
+
+    return ipv6
+
+def updateIp(newIp):
+    client = ovh.Client()
+
+    recordId = client.get("/domain/zone/ppom.me/record?fieldType=AAAA")
+    if not len(recordId):
+        print(f"error no AAAA ppom.me. record", file=sys.stderr)
+        sys.exit(1)
+    recordId = recordId[0]
+
+    recordData = client.get(f"/domain/zone/ppom.me/record/{recordId}")
+    oldIp = recordData['target']
+
+    if oldIp != newIp:
+        client.put(f"/domain/zone/ppom.me/record/{recordId}", target=newIp)
+        client.post("/domain/zone/ppom.me/refresh")
+        print(f"update from {oldIp} to {newIp}")
+    else:
+        print(f"unchanged {oldIp}")
+
+updateIp(getIp())
