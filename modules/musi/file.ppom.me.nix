8f7aa0c7948dd288b4412707e26fe0c20f152ce7
ppom <musi@ppom.me>
2024-04-28 12:00:00 +0200

    [musi/nextcloud] bump to 28 + nextcloud-occ doas
    doas script not completely working

--- modules/musi/file.ppom.me.nix
+++ modules/musi/file.ppom.me.nix
@@ -1,75 +1,103 @@
 { lib, pkgs, config, ... }:
-let
-  var = import ../common/reaction-variables.nix { inherit pkgs; };
-in {
-  services.nextcloud = {
-    enable = true;
-    package = pkgs.nextcloud27;
-    configureRedis = true;
-    autoUpdateApps.enable = true;
-    hostName = "file.ppom.me";
-    https = true;
-    database.createLocally = true;
-    config = {
-      dbtype = "pgsql";
-      adminpassFile = "/var/secrets/file/admin";
-      adminuser = "admin";
-      # Not possible because services.nextcloud.config is not of freeform type
-      # mail_smtpmode = "smtp";
-      # mail_smtphost = "smtp.ecomail.fr";
-      # mail_smtpsecure = "ssl";
-      # mail_smtpauthtype = "LOGIN";
-      # mail_smtpname     = "paco@ecomail.io";
-      # mail_smtppassword = "' . trim(file_get_contents('/var/secrets/mail/ecomail')) . '";
+lib.mkMerge [
+  # Nextcloud
+  {
+    services.nextcloud = {
+      enable = true;
+      package = pkgs.nextcloud28;
+      configureRedis = true;
+      autoUpdateApps.enable = true;
+      hostName = "file.ppom.me";
+      https = true;
+      database.createLocally = true;
+      config = {
+        dbtype = "pgsql";
+        adminpassFile = "/var/secrets/file/admin";
+        adminuser = "admin";
+        # Not possible because services.nextcloud.config is not of freeform type
+        # mail_smtpmode = "smtp";
+        # mail_smtphost = "smtp.ecomail.fr";
+        # mail_smtpsecure = "ssl";
+        # mail_smtpauthtype = "LOGIN";
+        # mail_smtpname     = "paco@ecomail.io";
+        # mail_smtppassword = "' . trim(file_get_contents('/var/secrets/mail/ecomail')) . '";
+      };
+      phpOptions = {
+        "pm.start_servers" = "4";
+        "opcache.interned_strings_buffer" = "12";
+      };
     };
-    phpOptions = {
-      "pm.start_servers" = "4";
+
+    users.users.nextcloud.extraGroups = [ "postmaster" ];
+
+    services.nginx.virtualHosts = {
+      "file.ppom.me" = {
+        enableACME = true;
+        forceSSL = true;
+      };
     };
-  };
 
-  users.users.nextcloud.extraGroups = [ "postmaster" ];
+    services.postgresqlBackup.databases = [ "nextcloud" ];
 
-  services.nginx.virtualHosts = {
-    "file.ppom.me" = {
-      enableACME = true;
-      forceSSL = true;
+    services.reaction.settings.streams.nextcloud = let
+      var = import ../common/reaction-variables.nix { inherit pkgs; };
+    in {
+      cmd = [ var.journalctl "-fn0" "-u" "phpfpm-nextcloud.service" ];
+      filters.failedLogin = {
+        regex = [
+          ''"remoteAddr":"<ip>".*"message":"Login failed:''
+          ''"remoteAddr":"<ip>".*"message":"Trusted domain error.''
+        ];
+        retry = 3;
+        retryperiod = "1h";
+        actions = var.banFor "1h";
+      };
     };
-  };
+  }
 
-  services.postgresqlBackup.databases = [ "nextcloud" ];
-
-  services.reaction.settings.streams.nextcloud = {
-    cmd = [ var.journalctl "-fn0" "-u" "phpfpm-nextcloud.service" ];
-    filters.failedLogin = {
-      regex = [
-        ''"remoteAddr":"<ip>".*"message":"Login failed:''
-        ''"remoteAddr":"<ip>".*"message":"Trusted domain error.''
-      ];
-      retry = 3;
-      retryperiod = "1h";
-      actions = var.banFor "1h";
-    };
-  };
-
-  users.users.cospend-balance = {
-    isSystemUser = true;
-    group = "cospend-balance";
-  };
-  users.groups.cospend-balance = {};
-
-  systemd.services.cospend-balance = let
-    package = pkgs.callPackage ../../pkgs/cospend-balance {};
-    settings = ./cospend-balance.yml;
+  # doas/sudo workaround for nextcloud-occ
+  (let
+    execstart = lib.splitString " " config.systemd.services.nextcloud-cron.serviceConfig.ExecStart;
+    php = "${lib.head execstart}";
+    webroot = builtins.dirOf (lib.elemAt execstart 2);
   in {
-    enable = true;
-    description = "Sync Nextcloud Cospend bills";
-    requires = ["phpfpm-nextcloud.service"];
-    after = ["phpfpm-nextcloud.service"];
-    startAt = "02:55";
-    serviceConfig = {
-      User = "cospend-balance";
-      Group = "cospend-balance";
-      ExecStart = "${package}/bin/cospend-balance -y ${settings}";
+    environment.systemPackages = [
+      (lib.hiPrio (pkgs.writeShellScriptBin "nextcloud-occ" ''
+        cd ${webroot}
+        export NEXTCLOUD_CONFIG_DIR="/var/lib/nextcloud/config"
+        exec /run/wrappers/bin/doas -u nextcloud ${php} occ "$@"
+      ''))
+    ];
+    security.doas.extraRules = [ {
+      cmd = php;
+      setEnv = [ "NEXTCLOUD_CONFIG_DIR" ];
+      runAs = "nextcloud";
+      groups = [ "wheel" ];
+    } ];
+  })
+
+  # Cospend balance
+  {
+    users.users.cospend-balance = {
+      isSystemUser = true;
+      group = "cospend-balance";
     };
-  };
-}
+    users.groups.cospend-balance = {};
+
+    systemd.services.cospend-balance = let
+      package = pkgs.callPackage ../../pkgs/cospend-balance {};
+      settings = ./cospend-balance.yml;
+    in {
+      enable = true;
+      description = "Sync Nextcloud Cospend bills";
+      requires = ["phpfpm-nextcloud.service"];
+      after = ["phpfpm-nextcloud.service"];
+      startAt = "02:55";
+      serviceConfig = {
+        User = "cospend-balance";
+        Group = "cospend-balance";
+        ExecStart = "${package}/bin/cospend-balance -y ${settings}";
+      };
+    };
+  }
+]
