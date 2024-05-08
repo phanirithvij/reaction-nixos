395d57c5d51e3fc3e1258b36d74b5f1aae701411
ppom <musi@ppom.me>
2024-05-08 12:00:00 +0200

    [musi/directus2zola] update lili-bel.com push_url

--- modules/musi/edit.ppom.me/lili-bel.com.nix
+++ modules/musi/edit.ppom.me/lili-bel.com.nix
@@ -4,6 +4,6 @@
     name = "lili-bel.com";
     script = [ "${pkgs.deno}/bin/deno" "run" "--allow-net" "./content.js" ];
     git_url = "https://framagit.org/ppom/lili-bel.com.git";
-    push_url = "musi-uploader@akesi.ppom.me:/var/www/static/lili";
+    push_url = "musi-uploader@akesi.ppom.me:/var/www/lili-bel.com";
   }];
 }
