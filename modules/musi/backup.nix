3fd52da55ed679f9d400ea1a28f9ce69c0b449f2
ppom <musi@ppom.me>
2024-09-12 12:00:00 +0200

    [musi/backup] remove first backup repository

--- modules/musi/backup.nix
+++ modules/musi/backup.nix
@@ -14,28 +14,28 @@ in {
     calendar = "*-*-* 0/6:15";
   };
 
-  services.restic.backups.data = {
-    paths = [ "/data/" "/var/" "/etc/nixos/" "/home/" "/root/" "/nix/var/nix/" ];
-    passwordFile = "/var/secrets/backups/data/pass";
-    extraOptions = [
-      "sftp.command='ssh ppom@node.cmercier.fr -i /var/secrets/backups/data/sshkey -s sftp'"
-    ];
-    repository = "sftp:ppom@node.cmercier.fr:/pacobackup/data";
-    initialize = true;
-    pruneOpts = [
-      "--keep-daily 7"
-      "--keep-weekly 5"
-      "--keep-monthly 12"
-    ];
-    exclude = [
-      "/var/cache"
-      "/home/*/.cache"
-    ];
-    timerConfig = {
-      OnCalendar = [ "02:00" ];
-      RandomizedDelaySec = "30m";
-    };
-  };
+  # services.restic.backups.data = {
+  #   paths = [ "/data/" "/var/" "/etc/nixos/" "/home/" "/root/" "/nix/var/nix/" ];
+  #   passwordFile = "/var/secrets/backups/data/pass";
+  #   extraOptions = [
+  #     "sftp.command='ssh ppom@node.cmercier.fr -i /var/secrets/backups/data/sshkey -s sftp'"
+  #   ];
+  #   repository = "sftp:ppom@node.cmercier.fr:/pacobackup/data";
+  #   initialize = true;
+  #   pruneOpts = [
+  #     "--keep-daily 7"
+  #     "--keep-weekly 5"
+  #     "--keep-monthly 12"
+  #   ];
+  #   exclude = [
+  #     "/var/cache"
+  #     "/home/*/.cache"
+  #   ];
+  #   timerConfig = {
+  #     OnCalendar = [ "02:00" ];
+  #     RandomizedDelaySec = "30m";
+  #   };
+  # };
 
   services.restic.backups.data2 = let
     pokiHost = "musi@192.168.1.74";
