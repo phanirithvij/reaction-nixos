0bb2ebc11859be80503a6e8ceef58e5a22bfd3a1
ppom <sona@ppom.me>
2024-11-07 12:00:00 +0100

    [sona/down-detector] improve with live notifications

--- modules/sona/down-detector.nix
+++ modules/sona/down-detector.nix
@@ -1,14 +1,29 @@
 { lib, config, pkgs, ... }:
 let
   websites = [
-    "https://video.ppom.me"
-    "https://music.ppom.me"
+    "https://blog.ppom.me"
+    "https://cours.ppom.fr"
+    "https://edit.ppom.fr"
+    "https://edit.ppom.me/leborddeleau/admin"
+    "https://edit.ppom.me/pompeani.art/admin"
+    "https://fesse.cloud"
     "https://file.ppom.me"
-    "https://akesi.ppom.me"
+    "https://lili-bel.com"
+    "https://music.ppom.me"
+    "https://paris-loyers.fr"
+    "https://pompeani.art"
+    "https://ppom.fr"
+    "https://static.ppom.fr"
+    "https://tokipona.ppom.me"
+    "https://u.ppom.me"
+    "https://video.ppom.me"
   ];
   dns_test = "dns2.proxad.net";
   log_file = "/home/ao/DOWN";
   down_detector = pkgs.writeShellScriptBin "down_detector.sh" ''
+    export DISPLAY=${"\$"}{DISPLAY:=":0"}
+    export XDG_RUNTIME_DIR=${"\$"}{XDG_RUNTIME_DIR:=/run/user/$(id -u)}
+    export DBUS_SESSION_BUS_ADDRESS=${"\$"}{DBUS_SESSION_BUS_ADDRESS:="unix:path=${"\$"}{XDG_RUNTIME_DIR}/bus"}
     LOG_FILE="${log_file}"
 
     SITES="${lib.concatStringsSep " " websites}"
@@ -28,15 +43,11 @@ let
             if [[ $STATUS -ne 0 ]]
             then
                 echo "$(date): $site is down! curl code = $STATUS" >> "$LOG_FILE"
-                FAIL=1
+                ${pkgs.libnotify}/bin/notify-send --urgency=critical --expire-time 3000 "$site is down!" "curl code = $STATUS"
             fi
         done
 
         ${pkgs.coreutils}/bin/rm -r "$DIR"
-        exit $FAIL
-
-    else
-      exit 3
     fi
   '';
 in
