86a8612d8669f8ddc8e5cccc351ac657bde2d4fc
ppom <musi@ppom.me>
2024-11-07 12:00:00 +0100

    [musi/backup] fix typos

--- modules/musi/backup.nix
+++ modules/musi/backup.nix
@@ -1,6 +1,6 @@
 { lib, pkgs, config, ... }:
 let
-  hosts = builtins.fromTOML (builtins.readFile ./hosts.toml);
+  hosts = builtins.fromTOML (builtins.readFile ../common/hosts.toml);
 in {
   services.postgresqlBackup = {
     enable = true;
@@ -33,7 +33,7 @@ in {
 
   services.restic.backups.data2 = let
     kiliHost = "musi@${hosts.kili.address}";
-    pokiHost = "musi@${hosts.musi.address}";
+    pokiHost = "musi@${hosts.poki.address}";
     pokiKey = "/var/secrets/backups/data2/sshkey";
   in {
     paths = [ "/data/" "/var/" "/etc/nixos/" "/home/" "/root/" "/nix/var/nix/" ];
@@ -57,7 +57,7 @@ in {
       RandomizedDelaySec = "30m";
     };
     backupPrepareCommand = "${pkgs.writeShellScript "wake-poki" ''
-      ssh ${kiliHost} -i ${pokiKey} wakelan A0:B3:CC:E9:4C:9C || exit 1
+      ssh ${kiliHost} -i ${pokiKey} wakeonlan A0:B3:CC:E9:4C:9C || exit 1
       for _ in $(seq 60)
       do
         sleep 15
