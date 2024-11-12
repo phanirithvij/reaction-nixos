10188a85e45a8abe0ccf4f2b67bc3531633d1790
ppom <musi@ppom.me>
2024-11-12 12:00:00 +0100

    [musi/tor] disable

--- modules/musi/tor.nix
+++ modules/musi/tor.nix
@@ -3,20 +3,20 @@ let
   torPort = 666;
 in
 {
-  networking.firewall.allowedTCPPorts = [ torPort ];
+  # networking.firewall.allowedTCPPorts = [ torPort ];
 
-  services.tor = {
-    enable = true;
-    enableGeoIP = true;
-    relay = {
-      enable = true;
-      role = "relay";
-    };
-    settings = {
-      Nickname = "parpaing";
-      BandwidthRate = 10 * 1024 * 1024; # 10 MB/s
-      ContactInfo = "parpaing@tuta.io";
-      ORPort = torPort;
-    };
-  };
+  # services.tor = {
+  #   enable = true;
+  #   enableGeoIP = true;
+  #   relay = {
+  #     enable = true;
+  #     role = "relay";
+  #   };
+  #   settings = {
+  #     Nickname = "parpaing";
+  #     BandwidthRate = 10 * 1024 * 1024; # 10 MB/s
+  #     ContactInfo = "parpaing@tuta.io";
+  #     ORPort = torPort;
+  #   };
+  # };
 }
