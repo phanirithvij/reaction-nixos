1ce2478ee96a6479b456a3b2fd25740f8abd3e63
ppom <sona@ppom.me>
2024-12-02 12:00:00 +0100

    [sona/syncthing] open port

--- modules/sona/syncthing.nix
+++ modules/sona/syncthing.nix
@@ -2,7 +2,7 @@
 {
   environment.systemPackages = [ pkgs.syncthing ];
   networking.firewall = {
-    allowedTCPPorts = [ 22000 ];
+    allowedTCPPorts = [ 8384 22000 ];
     allowedUDPPorts = [ 21027 22000 ];
   };
 }
