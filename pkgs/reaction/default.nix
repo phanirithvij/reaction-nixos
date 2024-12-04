8041994efb035ad4e9392efc1a689b82f9d5c0f4
ppom <musi@ppom.me>
2024-12-04 12:00:00 +0100

    [reaction] update dev version

--- pkgs/reaction/default.nix
+++ pkgs/reaction/default.nix
@@ -7,17 +7,17 @@ stdenv
 let
   pname = "reaction";
   # version = "v2.0.0-rc1";
-  version = "b747e52e94e5698090ddb924490eaa4318e127eb";
+  version = "7c3116b7c901ca2b9bd39264faba25128ca6c45e";
   src = fetchFromGitLab {
     domain = "framagit.org";
     owner = "ppom";
     repo = pname;
     rev = version;
-    sha256 = "sha256-B+jxity7ztbLAoUYu3MXLlyvKaDCY5bO7WG6/59eQeQ=";
+    sha256 = "sha256-oxVeCss+Y/G2HnDB8teWPe2FNdXbkQBysZbJ1aXH/LU=";
   };
   reaction = rustPlatform.buildRustPackage {
     inherit pname version src;
-    cargoSha256 = "sha256-gA4SZDA7yJQM7RWpowI12nvUnbKVA0Y2XSqlhJ4fewQ=";
+    cargoSha256 = "sha256-LX8lI4GZpH62JCgK7brmI8KpM1aAFEoTxC7sBTiswMk=";
   };
   ip46tables = stdenv.mkDerivation {
     inherit version src;
