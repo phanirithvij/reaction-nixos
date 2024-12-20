fb44c5663e965f2cfc6beef0ba9e833891e7a4ac
ppom <sona@ppom.me>
2024-12-20 12:00:00 +0100

    [sona/virt] libvirt commented conf

--- modules/sona/virt.nix
+++ modules/sona/virt.nix
@@ -10,6 +10,18 @@
   virtualisation.docker.enableOnBoot = false;
   systemd.services.docker.wantedBy = lib.mkForce [];
 
+  # virtualisation.libvirtd = {
+  #   enable = true;
+  #   qemu = {
+  #     package = pkgs.qemu_kvm;
+  #     ovmf = {
+  #       enable = true;
+  #       packages = [pkgs.OVMFFull.fd];
+  #     };
+  #     swtpm.enable = true;
+  #   };
+  # };
+
   # virtualisation.lxd.enable = true;
   # systemd.services.lxd.wantedBy = lib.mkForce [];
 
