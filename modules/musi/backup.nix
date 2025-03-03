4c2b78e7f9c6dc4f198914b2518c5bb12c9133ef
ppom <musi@ppom.me>
2025-03-03 12:00:00 +0100

    [musi/backup] fix backups when poki is up but kili is down

--- modules/musi/backup.nix
+++ modules/musi/backup.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, config, ... }:
+{ pkgs, ... }:
 let
   hosts = builtins.fromTOML (builtins.readFile ../common/hosts.toml);
 in {
@@ -57,7 +57,7 @@ in {
       RandomizedDelaySec = "30m";
     };
     backupPrepareCommand = "${pkgs.writeShellScript "wake-poki" ''
-      ssh ${kiliHost} -i ${pokiKey} wakeonlan A0:B3:CC:E9:4C:9C || exit 1
+      ssh ${kiliHost} -i ${pokiKey} wakeonlan A0:B3:CC:E9:4C:9C || exit 0
       for _ in $(seq 60)
       do
         sleep 15
