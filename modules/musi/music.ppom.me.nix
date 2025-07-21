32ba1a1444889a501f938e0867ae8bfbff7b4b66
ppom <musi@ppom.me>
2025-07-21 12:00:00 +0200

    [musi/music] add meelo host

--- modules/musi/music.ppom.me.nix
+++ modules/musi/music.ppom.me.nix
@@ -128,4 +128,13 @@ in {
       '';
     };
   };
+
+  services.nginx.virtualHosts."meelo.ppom.me" = {
+    forceSSL = true;
+    enableACME = true;
+    locations."/" = {
+      proxyPass = "http://localhost:8942";
+      proxyWebsockets = true;
+    };
+  };
 }
